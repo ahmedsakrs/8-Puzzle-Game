e02c45bbff05a398c92ee41e709cc68233ac33aa
Ahmed Sakr <asakr1191@gmail.com>
2022-11-18 17:03:29 +0200

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,231 +1,175 @@
-from frontend import Ui_MainWindow
-from algorithms import *
-from PyQt5 import QtWidgets
 import random
+import time
+
+from PyQt5.QtCore import QTimer
+from frontend import Ui_MainWindow
+from PyQt5 import QtWidgets
+from algorithms import *
 
 
-class PuzzleSolver(QtWidgets.QMainWindow):
+class CONNECT4(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
-
-        self.state = None
-        self.mode = None
-        self.path = None
-        self.i = 0
-
         self.ui.setupUi(self)
-        self.ui.randomFrame.hide()
-        self.ui.stateFrame.hide()
-        self.ui.algorithmBox.hide()
-        self.ui.heuristicGroup.hide()
-        self.ui.solveButton.hide()
-        self.ui.pathFrame.hide()
-        self.ui.prevButton.hide()
-        self.ui.nextButton.hide()
-        self.ui.costLabel.hide()
-        self.ui.runtimeLabel.hide()
-        self.ui.depthLabel.hide()
-        self.ui.expandedLabel.hide()
 
-        self.ui.randomButton.clicked.connect(self.initialize_random)
-        self.ui.stateButton.clicked.connect(self.get_input)
-        self.ui.bfsRadio.clicked.connect(self.show_solve_hide_heuristic)
-        self.ui.dfsRadio.clicked.connect(self.show_solve_hide_heuristic)
-        self.ui.aRadio.clicked.connect(self.show_heuristic)
-        self.ui.manhattanRadio.clicked.connect(self.show_solve)
-        self.ui.euclideanRadio.clicked.connect(self.show_solve)
-        self.ui.solveButton.clicked.connect(self.solve)
-        self.ui.nextButton.clicked.connect(self.next)
-        self.ui.prevButton.clicked.connect(self.prev)
+        self.blinked = False
+        self.state = [['0' for _ in range(7)] for _ in range(6)]
+        self.turn = random.randrange(1, 3, 1)  # 1 for user's turn, 2 for computer turn
+        if self.turn == 1:
+            self.ui.turn.setStyleSheet("background-color: rgb(255, 0, 0); border-radius: 50px")
 
-    def next(self) -> None:
-        self.i += 1
-        self.ui.progressLabel.setText('State ' + str(self.i) + ' / ' + str(len(self.path) - 1))
-        self.ui.prevButton.setEnabled(True)
-        if self.i == len(self.path) - 1:
-            self.ui.nextButton.setEnabled(False)
-        self.display_path()
+        if self.turn == 2:
+            self.ui.frame.setEnabled(False)
 
-    def prev(self) -> None:
-        self.i -= 1
-        self.ui.progressLabel.setText('State ' + str(self.i) + ' / ' + str(len(self.path) - 1))
-        self.ui.nextButton.setEnabled(True)
-        if self.i == 0:
-            self.ui.prevButton.setEnabled(False)
-        self.display_path()
+        self.timer = QTimer(self)
+        self.timer.start(500)
 
-    def solve(self) -> None:
-        self.i = 0
-        if self.mode == 'input':
-            valid = self.validate()
-            if not valid:
-                return
+        self.ui.reset.clicked.connect(self.reset)
+        self.ui.noPruning.toggle()
 
-        self.ui.progressLabel.show()
-        self.ui.nextButton.setEnabled(True)
-        self.ui.prevButton.setEnabled(False)
-        self.ui.expandedLabel.show()
-        self.ui.depthLabel.show()
-        self.ui.runtimeLabel.show()
-        self.ui.runtimeResult.show()
-        self.ui.depthResult.show()
-        self.ui.expandedResult.show()
+        self.col0 = [self.ui.button0, self.ui.button7, self.ui.button14, self.ui.button21, self.ui.button28,
+                     self.ui.button35]
+        self.col1 = [self.ui.button1, self.ui.button8, self.ui.button15, self.ui.button22, self.ui.button29,
+                     self.ui.button36]
+        self.col2 = [self.ui.button2, self.ui.button9, self.ui.button16, self.ui.button23, self.ui.button30,
+                     self.ui.button37]
+        self.col3 = [self.ui.button3, self.ui.button10, self.ui.button17, self.ui.button24, self.ui.button31,
+                     self.ui.button38]
+        self.col4 = [self.ui.button4, self.ui.button11, self.ui.button18, self.ui.button25, self.ui.button32,
+                     self.ui.button39]
+        self.col5 = [self.ui.button5, self.ui.button12, self.ui.button19, self.ui.button26, self.ui.button33,
+                     self.ui.button40]
+        self.col6 = [self.ui.button6, self.ui.button13, self.ui.button20, self.ui.button27, self.ui.button34,
+                     self.ui.button41]
+        self.cols = [self.col0, self.col1, self.col2, self.col3, self.col4, self.col5, self.col6]
 
-        result = None
+        self.timer.timeout.connect(self.blink)
+        self.ui.button0.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button1.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button2.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button3.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button4.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button5.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button6.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button7.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button8.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button9.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button10.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button11.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button12.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button13.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button14.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button15.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button16.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button17.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button18.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button19.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button20.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button21.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button22.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button23.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button24.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button25.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button26.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button27.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button28.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button29.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button30.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button31.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button32.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button33.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button34.clicked.connect(lambda: self.set_state(6, str(self.turn)))
+        self.ui.button35.clicked.connect(lambda: self.set_state(0, str(self.turn)))
+        self.ui.button36.clicked.connect(lambda: self.set_state(1, str(self.turn)))
+        self.ui.button37.clicked.connect(lambda: self.set_state(2, str(self.turn)))
+        self.ui.button38.clicked.connect(lambda: self.set_state(3, str(self.turn)))
+        self.ui.button39.clicked.connect(lambda: self.set_state(4, str(self.turn)))
+        self.ui.button40.clicked.connect(lambda: self.set_state(5, str(self.turn)))
+        self.ui.button41.clicked.connect(lambda: self.set_state(6, str(self.turn)))
 
-        if self.ui.bfsRadio.isChecked():
-            result = BFS(self.state)
+    def set_state(self, j, char):
+        if char == '1':
+            self.ui.frame.setEnabled(False)
+            for i in range(6):
+                if self.state[i][j] == '0':
+                    self.state[i][j] = char
+                    self.display_state()
+                    self.turn = 2
+                    self.ui.turn.setStyleSheet("background-color: rgb(255, 255, 0); border-radius: 50px")
+                    k = self.ui.maxDepth.value()
+                    start = time.time()
+                    self.display_state()
+                    self.disable()
+                    self.state = minimax(self.state, k, self.ui.pruning.isChecked(), self.ui.searchTree.isChecked())[0]
+                    self.ui.turn.setStyleSheet("background-color: rgb(255, 0, 0); border-radius: 50px")
+                    end = time.time()
+                    self.turn = 1
+                    self.display_state()
+                    self.disable()
+                    self.ui.frame.setEnabled(True)
+                    print(end - start)
+                    return
 
-        elif self.ui.dfsRadio.isChecked():
-            result = DFS(self.state)
+    def disable(self):
+        for j, col in enumerate(self.cols):
+            if self.state[-1][j] != '0':
+                for button in col:
+                    button.setEnabled(False)
 
-        elif self.ui.aRadio.isChecked():
-            heuristic = 0 if self.ui.manhattanRadio.isChecked() else 1
-            result = A(self.state, heuristic)
+        if is_full(self.state):
+            self.timer.stop()
+            self.ui.turn.hide()
 
-        if len(result) == 3:
-            self.ui.pathFrame.hide()
-            self.ui.prevButton.hide()
-            self.ui.nextButton.hide()
-            self.ui.costLabel.hide()
-            self.ui.costResult.hide()
-            self.ui.runtimeResult.setText(str(result[2]) + ' sec')
-            self.ui.depthResult.setText(str(result[1]))
-            self.ui.expandedResult.setText(str(result[0]))
-            self.ui.progressLabel.setText('This State has no path to the goal')
-            return
-        self.path, cost, explored, max_depth, runtime = result
+    def display_state(self):
+        for j, col in enumerate([self.col0, self.col1, self.col2, self.col3, self.col4, self.col5, self.col6]):
+            for i, button in enumerate(col):
+                if self.state[i][j] == '0':
+                    button.setStyleSheet("background-color: rgb(206, 206, 206); border-radius: 50px")
+                elif self.state[i][j] == '1':
+                    button.setStyleSheet("background-color: rgb(255, 0, 0); border-radius: 50px")
+                else:
+                    button.setStyleSheet("background-color: rgb(255, 255, 0); border-radius: 50px")
+        agent, user = getScore(self.state)
+        self.ui.agent_score.setText('Agent Score:    ' + str(agent))
+        self.ui.user_score.setText('Your Score:    ' + str(user))
 
-        self.ui.pathFrame.show()
-        self.ui.nextButton.show()
-        self.ui.prevButton.show()
-        self.ui.costLabel.show()
-        self.ui.costResult.show()
-
-        self.ui.costResult.setText(str(cost))
-        self.ui.expandedResult.setText(str(explored))
-        self.ui.depthResult.setText(str(max_depth))
-        self.ui.runtimeResult.setText(str(runtime) + ' sec')
-        self.ui.progressLabel.setText('State ' + str(self.i) + ' / ' + str(len(self.path) - 1))
-
-        if self.state == 12345678:
-            self.ui.nextButton.setEnabled(False)
-            self.ui.prevButton.setEnabled(False)
-
-        self.display_path()
-
-    def display_path(self) -> None:
-        current = list(str(self.path[self.i]))
-        if len(current) == 8:
-            current.insert(0, '')
+    def blink(self):
+        if self.blinked:
+            self.ui.turn.show()
+            self.blinked = not self.blinked
         else:
-            i = current.index('0')
-            current[i] = ''
+            self.ui.turn.hide()
+            self.blinked = not self.blinked
 
-        self.ui.result0.setText(current[0])
-        self.ui.result1.setText(current[1])
-        self.ui.result2.setText(current[2])
-        self.ui.result3.setText(current[3])
-        self.ui.result4.setText(current[4])
-        self.ui.result5.setText(current[5])
-        self.ui.result6.setText(current[6])
-        self.ui.result7.setText(current[7])
-        self.ui.result8.setText(current[8])
-
-    def validate(self) -> bool:
-        self.state = []
-        self.state.append(self.ui.text0.text())
-        self.state.append(self.ui.text1.text())
-        self.state.append(self.ui.text2.text())
-        self.state.append(self.ui.text3.text())
-        self.state.append(self.ui.text4.text())
-        self.state.append(self.ui.text5.text())
-        self.state.append(self.ui.text6.text())
-        self.state.append(self.ui.text7.text())
-        self.state.append(self.ui.text8.text())
-
-        valid = {'', '1', '2', '3', '4', '5', '6', '7', '8'}
-        if valid - set(self.state) == set():
-            index = self.state.index('')
-            self.state[index] = '0'
-            self.state = int(''.join(self.state))
-            return True
-
-        msg = QtWidgets.QMessageBox()
-        msg.setIcon(QtWidgets.QMessageBox.Critical)
-        msg.setText("Error")
-        msg.setInformativeText('Insert all values 1 - 8\nLeave only one empty block')
-        msg.setWindowTitle("Error")
-        msg.exec_()
-        return False
-
-    def show_solve_hide_heuristic(self) -> None:
-        self.ui.heuristicGroup.hide()
-        self.ui.solveButton.show()
-
-    def get_input(self) -> None:
-        self.ui.algorithmBox.show()
-        self.ui.randomFrame.hide()
-        self.ui.stateFrame.show()
-        self.ui.text0.setText('')
-        self.ui.text1.setText('')
-        self.ui.text2.setText('')
-        self.ui.text3.setText('')
-        self.ui.text4.setText('')
-        self.ui.text5.setText('')
-        self.ui.text6.setText('')
-        self.ui.text7.setText('')
-        self.ui.text8.setText('')
-        self.mode = 'input'
-
-    def show_solve(self) -> None:
-        self.ui.solveButton.show()
-
-    def show_heuristic(self) -> None:
-        if not self.ui.euclideanRadio.isChecked() and not self.ui.manhattanRadio.isChecked():
-            self.ui.solveButton.hide()
+    def reset(self):
+        self.state = [['0' for _ in range(7)] for _ in range(6)]
+        self.turn = random.randrange(1, 3, 1)  # 1 for user's turn, 2 for computer turn
+        if self.turn == 1:
+            self.ui.turn.setStyleSheet("background-color: rgb(255, 0, 0); border-radius: 50px")
         else:
-            self.ui.solveButton.show()
-        self.ui.heuristicGroup.show()
+            self.ui.turn.setStyleSheet("background-color: rgb(255, 255, 0); border-radius: 50px")
 
-    def initialize_random(self) -> None:
-        self.ui.stateFrame.hide()
-        self.ui.randomFrame.show()
-        self.mode = 'random'
+        if self.turn == 2:
+            self.ui.frame.setEnabled(False)
+        else:
+            self.ui.frame.setEnabled(True)
 
-        self.state = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
-        random.shuffle(self.state)
+        row = [self.ui.button35, self.ui.button36, self.ui.button37, self.ui.button38, self.ui.button39,
+               self.ui.button40, self.ui.button41]
+
+        for button, col in zip(row, self.cols):
+            if not button.isEnabled():
+                for disabled_button in col:
+                    disabled_button.setEnabled(True)
+
         self.display_state()
-        self.state = int(''.join(self.state))
-
-        self.ui.algorithmBox.show()
-
-    def display_state(self) -> None:
-        index = 0
-        for i in range(len(self.state)):
-            if self.state[i] == '0':
-                self.state[i] = ''
-                index = i
-                break
-
-        self.ui.label0.setText(self.state[0])
-        self.ui.label1.setText(self.state[1])
-        self.ui.label2.setText(self.state[2])
-        self.ui.label3.setText(self.state[3])
-        self.ui.label4.setText(self.state[4])
-        self.ui.label5.setText(self.state[5])
-        self.ui.label6.setText(self.state[6])
-        self.ui.label7.setText(self.state[7])
-        self.ui.label8.setText(self.state[8])
-
-        self.state[index] = '0'
 
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    gui = PuzzleSolver()
+    gui = CONNECT4()
     gui.show()
     sys.exit(app.exec())
